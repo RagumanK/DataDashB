7d85fa54f6355b53f15e308f1d099a1d005c3c36
Raguman K <rrst1010@gmail.com>
2024-08-13 00:03:14 -0400

    Setup Base Styles for the application & build landing page.

---
+++ frontend/my-app/src/components/Header.svelte
@@ -0,0 +1,24 @@
+<script>
+</script>
+
+<header class="flex flex-col relative z-20">
+  <div class="max-w-[1400px] mx-auto w-full flex items-center justify-between p-4 py-6">
+    <a href="/">
+      <h1 class="font-semibold">
+        Liquid <span class="text-indigo-400">Analytics</span>
+      </h1>
+    </a>
+    <!-- on:click={() => ($openModal = true)} -->
+    <button class="md:hidden grid place-items-center">
+      <i class="fa-solid fa-bars"></i>
+    </button>
+    <nav class="hidden md:flex items-center gap-4 lg:gap-6">
+      <a href="/orders" class="duration-200 hover:text-indigo-400 cursor-pointer">Orders</a>
+      <a href="#reviews" class="duration-200 hover:text-indigo-400 cursor-pointer">Reviews</a>
+      <a href="#faqs" class="duration-200 hover:text-indigo-400 cursor-pointer">FAQs</a>
+      <button class="specialBtn">
+        <p>Start free today</p>
+      </button>
+    </nav>
+  </div>
+</header>
---
+++ frontend/my-app/src/components/Hero.svelte
@@ -0,0 +1,30 @@
+<script>
+  import SectionWrapper from "./SectionWrapper.svelte";
+  import CtAs from "./CTAs.svelte";
+  import Stars from "./Stars.svelte";
+</script>
+
+<SectionWrapper>
+  <div class="flex flex-col gap-10 flex-1 items-center justify-center pb-10 md:pb-14">
+    <h2 class="text-5xl sm:text-6xl md:text-7xl lg:text-8xl max-w-[1200px] mx-auto w-full text-center font-semibold">
+      <span class="text-indigo-400">Data</span> Visualization
+      <br />
+      <span class="text-slate-600 line-through">Is Hard</span><br />
+      Just Got <span class="text-indigo-400">Easier</span>
+    </h2>
+    <p class="text-xl sm:text-2xl md:text-3xl text-center max-w-[1000px] mx-auto w-full">
+      Turn insights into action. We're the analyst in your pocket, offering <span class="italic"
+        >customized visualizations, data explanations, analytics</span
+      > and much more.
+    </p>
+    <div class="flex items-center gap-4">
+      <button class={"specialBtn"}>
+        <p class="text-base sm:text-lg md:text-xl">Contact Us</p>
+      </button>
+      <button class="specialBtnDark">
+        <p class="text-base sm:text-lg md:text-xl">Get Started &rarr;</p>
+      </button>
+    </div>
+    <Stars />
+  </div>
+</SectionWrapper>
